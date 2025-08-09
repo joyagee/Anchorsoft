f9a7c276184da3ee0381cd840c2ea9bedd688f0c
joyagee <joychayi66@gmail.com>
2025-08-09 13:21:21 +0100

    i added banner4 to my eko hotel

---
+++ Team_june2025/Intro/javascript/index.js
@@ -0,0 +1,28 @@
+console.log("hellow world");
+// Declaration
+let figure
+//console.log("figure:", figure);
+
+// Assignment
+figure = 20
+//console.log("figure:", figure);
+
+// Declaration And Assignment
+let number = 3
+//console.log("number:", number);
+//use of constant
+const amount = 20 
+let name = "Favour"
+console.log(Name);
+
+// ******primitive type*****
+
+// 1.Strings
+const student ="Delight"
+console.log("type of student:", typeof student);
+
+// 2.Number
+const amountS =20
+console.log("type of amountS :", typeof amountS);
+
+// 3.Boolean
